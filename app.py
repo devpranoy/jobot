8036acef1b0824c4780d704b2b7c6d8efd6a3785
Pranoy Dev <devpranoy@gmail.com>
2018-06-07 02:59:56 +0530

    post test

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def index():
-	return "Yo, it's working!"
+	if request.method == "POST":
+		name=request.form['name']
+		email=request.form['email']
+		subject=request.form['subject']
+		message=request.form['message']
+		return "Yo, it's working! "+name+email+subject+message
+	else
+		return "Yo it works!"
 
 if __name__ == "__main__":
 	app.run()
