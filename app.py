22677ff2da917fb87dcc7b596c11bada2d33953e
Pranoy Dev <devpranoy@gmail.com>
2018-06-07 03:42:38 +0530

    slack integration added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask ,render_template, flash, redirect, url_for, session, request, logging
-
+from slacker import Slacker
+slackClient =Slacker("xoxb-368007597783-376852833876-z57UPooZ3FHD1n50hOv46mmC")
 app = Flask(__name__)
 
 @app.route('/',methods=['GET', 'POST'])
@@ -9,9 +10,16 @@ def index():
 		email=request.form['Email']
 		subject=request.form['Subject']
 		message=request.form['Message']
-		return "Yo, it's working! "+name+email+subject+message
+		slackClient.chat.post_message("#general","boisss we have a customer")
+		slackClient.chat.post_message("#general","Name: "+name)
+		slackClient.chat.post_message("#general","Email: "+email)
+		slackClient.chat.post_message("#general","Subject: "+subject)
+		slackClient.chat.post_message("#general","Message: "+message)
+		return "Yo, it's working! "
 	else:
-		return "Yo it works!"
+		return "Yo it works!123"
+		messageToChannel = "Hey Martin, Ren Says Hi"
+		slackClient.chat.post_message("#general",messageToChannel)
 
 if __name__ == "__main__":
 	app.run()
